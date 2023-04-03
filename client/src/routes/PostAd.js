c9dd0b5bcc95f38a75cfda919f4ff53caf620f7e
eshitasharma <eshitasharma0804@gmail.com>
2023-04-03 13:48:31 -0700

    post-ad-functionality

--- client/src/routes/PostAd.js
+++ client/src/routes/PostAd.js
@@ -1,22 +1,53 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { ABI,contractAddress } from "../info/info";
 export default function PostAd() {
 const [itemName,setItemName]=useState('');
+const [items,setItems]=useState('');
+const [address, setAddress] = useState('');
 const [itemPrice,setItemPrice]=useState('');
 const [itemDescription,setItemDescription]=useState('');
 const [itemPickupLocation,setItemPickupLocation]=useState('');
+let contract;
+let provider;
+let signer;
   const handleSubmit = (e) => {
     e.preventDefault();
     const item ={itemName,itemPrice,itemDescription,itemPickupLocation};
-    console.log(item);
+    if(window.ethereum){
+      window.ethereum.request({method:"eth_requestAccounts"})
+      .then(result=>{
+      console.warn(result[0])
+      setAddress(result[0]);
+      })
+    }
+     provider = new ethers.providers.Web3Provider(window.ethereum);
+    provider.send("eth_requestAccounts", []);
+     signer = provider.getSigner();
+     const requestAccounts = async () => {
+      await provider.send("eth_requestAccounts", []);
+    } 
+    console.log(signer.address)
+    setAddress(requestAccounts[0]);
+    console.log(address);
+     contract = new ethers.Contract(contractAddress, ABI, signer);  
+     contract.postAd(item.itemName,item.itemDescription,item.itemPrice,item.itemPickupLocation);
+     x();
   }
+ 
+  const x = async () => {
+    const item=await contract.getAllItemIdsPostedByUser(address);
+    console.warn(await contract.getItem(item[2]))
+    //setItems(item);
 
+  } 
 interface Item { 
   itemName:string, 
   itemPrice:number, 
   itemDescription:string, 
   itemPickupLocation:string;
 } 
+
    return (
       <div class="form-container">
         <form class="register-form" onSubmit={handleSubmit} >
@@ -61,7 +92,9 @@ interface Item {
             Post
           </button>
         </form>
+        { console.log(contract)}
       </div>
+      
     );
-    
+
   }
