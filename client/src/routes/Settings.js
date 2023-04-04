c12a469f823f259f394241656a0e313efcebc2a1
eshitasharma <eshitasharma0804@gmail.com>
2023-04-03 19:35:59 -0700

    added-layout

--- client/src/routes/Settings.js
+++ client/src/routes/Settings.js
@@ -1,3 +1,10 @@
-export default function Settings() {
-    return <h1>Settings</h1>
+import { Link } from "react-router-dom";
+export default function Settings(props) {
+    return <div>
+       <Link to="/transactions">
+       <button className="button">
+        View Transaction History
+      </button>
+      </Link>
+    </div>
   }
---
+++ client/src/routes/TransactionsInfo.js
@@ -0,0 +1,13 @@
+
+export default function TransactionsInfo(props) {
+    return (
+        <div class="container">
+
+        <h1><i class="fa fa-exchange"></i> Transaction
+            <small>View information about an Ethereum transaction</small>
+        </h1>
+    
+    </div>
+    )
+  }
+  
