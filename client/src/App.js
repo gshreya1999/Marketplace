2819545f4fcb5d0d3bc42bbc09ae440e7e9fb883
eshitasharma <eshitasharma0804@gmail.com>
2023-04-03 19:53:36 -0700

    code-clean

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./routes/Home"
 import Settings from "./routes/Settings"
 import PostAd from "./routes/PostAd"
 import { Route, Routes } from "react-router-dom"
+import TransactionsInfo from './routes/TransactionsInfo';
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/post" element={<PostAd />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/transactions" element={<TransactionsInfo />} />
         </Routes>
       </div>
     </>
