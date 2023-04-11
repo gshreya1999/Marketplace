1f545582b2c2519d243d007d9b84981d7ab21a84
Mankirt <53979716+Mankirt@users.noreply.github.com>
2023-04-11 15:58:46 -0700

    Update Home.js

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,10 +1,11 @@
+
+
 import { useState, useEffect } from "react";
-import '../Home.css';
+import '../home.css';
 
 export default function Home(props) {
   const [itemStatus, setItemStatus] = useState(" Posted ");
-
-  const listings = [
+  const [listings, setListings] = useState([
     {
       itemName: "Blue Shirt",
       itemDescription: "A stylish blue shirt for any occasion",
@@ -13,7 +14,6 @@ export default function Home(props) {
       itemStatus: "Available",
       previews: [
         "bluet.jpg",
-        
       ],
     },
     {
@@ -23,8 +23,7 @@ export default function Home(props) {
       itemPickupLocation: "San Francisco, CA",
       itemStatus: "Available",
       previews: [
-        "bluet.jpg",
-       
+        "reds.jpg",
       ],
     },
     {
@@ -34,55 +33,56 @@ export default function Home(props) {
       itemPickupLocation: "Los Angeles, CA",
       itemStatus: "Available",
       previews: [
-        "bluet.jpg",
-      
+        "blackj.jpg",
       ],
     },
     {
-      itemName: "Blue Shirt",
-      itemDescription: "A stylish blue shirt for any occasion",
-      itemPrice: 0.5,
-      itemPickupLocation: "New York, NY",
-      itemStatus: "Available",
-      previews: [
-        "bluet.jpg",
-        
-      ],
-    },
-    {
-      itemName: "Red Sneakers",
-      itemDescription: "Comfortable red sneakers for running or walking",
+      itemName: "White Sneakers",
+      itemDescription: "Comfortable white sneakers for running or walking",
       itemPrice: 1.2,
       itemPickupLocation: "San Francisco, CA",
       itemStatus: "Available",
       previews: [
-        "bluet.jpg",
-       
+        "whites.jpg",
       ],
     },
     {
-      itemName: "Black Jeans",
-      itemDescription: "Classic black jeans for everyday wear",
+      itemName: "Navy Jeans",
+      itemDescription: "Classic navy jeans for everyday wear",
       itemPrice: 0.8,
       itemPickupLocation: "Los Angeles, CA",
       itemStatus: "Available",
       previews: [
-        "bluet.jpg",
-      
+        "navyj.jpg",
       ],
     },
-    // add more listings as needed
-  ];
+    {
+      itemName: "Red Shirt",
+      itemDescription: "A stylish red shirt for any occasion",
+      itemPrice: 2.4,
+      itemPickupLocation: "New York, NY",
+      itemStatus: "Available",
+      previews: [
+        "redt.jpg",
+      ],
+    },
+  ]);
   
-  function buyItem() {
+  function buyItem(index) {
     if(window.confirm('Are you sure you want to buy this item?')){
-      setItemStatus(" Sold ");
+      const updatedListings = [...listings];
+      updatedListings[index].itemStatus = "Sold";
+      setListings(updatedListings);
     }
   }
 
   function removeItem() {
     alert('Are you sure you want to remove this item?');
     setItemStatus(" Removed ");
+  }
+  
+  function handlePost(item) {
+    setListings([...listings, item]);
   }
   
   return (
@@ -116,9 +116,9 @@ export default function Home(props) {
                 </div>
               </div>
               <div className="productActions">
-                <button className="productActionButton" onClick={buyItem}>
+              <button className="button" onClick={() => buyItem(index)} disabled={listing.itemStatus === 'Sold'}>
                   <i className="fas fa-shopping-cart"></i>
-                  <span>Buy</span>
+                  Buy
                 </button>
                 
               </div>
@@ -126,40 +126,49 @@ export default function Home(props) {
           )}
         </div>
       ))}
+      <div className="product">
+     {props.itemName?.length >0  && 
+        <div key={props.itemName} className="productCard">
+          {props.previews &&
+            props.previews.map((pic) => {
+              return (
+                <img src={pic} alt="product-img" className="productImage"></img>
+              );
+            })}
+          <div>
+          <div className="productDetails">
+                <h3 className="productName">{props.itemName}</h3>
+                <p className="productDescription">{props.itemDescription}</p>
+                <div className="productPriceAndLocation">
+                  <span className="productPrice">Price: {props.itemPrice} ETH </span>
+                  <div className="productLocation">
+                    <i className="fas fa-map-marker-alt"></i>
+                    <span>{props.itemPickupLocation}</span>
+                  </div>
+                </div>
+                <div className={`productStatus productStatus--available`}>
+                  Available
+                </div>
+              </div>
+              <div className="productActions">
+                <button className="button" onClick={buyItem}>
+                  <i className="fas fa-shopping-cart"></i>
+                  Buy
+                </button>
+                
+              </div>
+            
+            
+          </div>
+        </div>
+}
+
+    </div>
     </div>
   
   
 
     
-//     <div className="product">
-//      {props.itemName?.length >0  && 
-//         <div key={props.itemName} className="productCard">
-//           {props.previews &&
-//             props.previews.map((pic) => {
-//               return (
-//                 <img src={pic} alt="product-img" className="productImage"></img>
-//               );
-//             })}
-//           <div>
-//             <h3 className="productName">{props.itemName}</h3>
-//             <p>{props.itemDescription}</p>
-//             <span>Price: {props.itemPrice} ETH </span>
-//             <div className="productTime">
-//               Pick up Location : {props.itemPickupLocation}
-//             </div>
-//             <h4>Status:{itemStatus} </h4>
-//           </div>
-//           <div>
-//             <button className="button" onClick={buyItem}>
-//               Buy
-//             </button>
-//             <button className="button" onClick={removeItem}>
-//               Remove
-//             </button>
-//           </div>
-//         </div>
-// }
-
-//     </div>
+    
   );
 }
