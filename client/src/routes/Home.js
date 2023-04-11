c72cbd6723267132af9836d22a7e32db0aa5fbc9
Shreya Gupta <124002231+gshreya1999@users.noreply.github.com>
2023-04-10 18:10:41 -0700

    Update Home.js

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -4,17 +4,25 @@ export default function Home(props) {
   const [itemStatus, setItemStatus] = useState(" Posted ");
 
   function buyItem() {
-    alert('Are you sure you want to do buy this item?');
-    setItemStatus(" Sold ");
+    if(itemStatus==" Posted ") {
+      alert('Are you sure you want to do buy this item?');
+      setItemStatus(" Sold ");
+    } else {
+      alert('This item cannot be bought because it is already sold or removed.');
+    }
   }
 
   function removeItem() {
-    alert('Are you sure you want to do remove this item?');
-    setItemStatus(" Removed ");
+    if (itemStatus === " Posted ") {
+      alert("Are you sure you want to remove this item?");
+      setItemStatus(" Removed ");
+    } else {
+      alert("This item cannot be removed because it is already sold or removed.");
+    }
   }
 
   return (
-    <div className="product">
+    <div style={{ display: "flex", justifyContent: "center" }}>
      {props.itemName?.length >0  && 
         <div key={props.itemName} className="productCard">
           {props.previews &&
