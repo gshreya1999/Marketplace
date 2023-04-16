08bf0ce9ebcda8866d76bd5a12219dfd172cd46f
eshitasharma <eshitasharma0804@gmail.com>
2023-04-15 22:32:16 -0700

    code-cleaning

--- client/src/routes/Settings.js
+++ client/src/routes/Settings.js
@@ -9,27 +9,27 @@ export default function Settings(prop) {
         <div className="col-lg-6 col-md-8 col-sm-10">
           <div className="d-grid gap-3">
             <Link to="/update-listing">
-              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px", marginTop: "25px" }}>
+              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px", marginTop: "25px",width:"50%",height:"50%" }}>
                 <i className="fas fa-pencil-alt"></i>Update a Listing
               </button>
             </Link>
             <Link to="/remove-listing">
-              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px"}}>
+              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px",width:"50%",height:"50%"}}>
                 <i className="far fa-trash-alt"></i>Remove a Listing
               </button>
             </Link>
             <Link to="/changepass">
-              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px"}}>
+              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px",width:"50%",height:"50%"}}>
                 <i className="fas fa-key"></i>Change Password
               </button>
             </Link>
             <Link to="/transactions">
-              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px"}}>
+              <button className="btn btn-primary btn-lg btn-custom" style={{ marginBottom: "50px", width:"50%",height:"50%"}}>
                 <i className="fas fa-history"></i>View Transaction History
               </button>
             </Link>
             <Link to="/delete-account">
-              <button className="btn btn-danger btn-lg">
+              <button className="btn btn-danger btn-lg" style={{ marginBottom: "50px", width:"50%",height:"50%"}}>
                 <i className="fas fa-trash-alt"></i>Delete Account
               </button>
             </Link>
